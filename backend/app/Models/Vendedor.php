8a533b32cd16e35939e68e58a7df5e15aaf4972a
Jonatan Gomez Acosta <gomezacosta21@gmail.com>
2026-03-08 16:35:37 -0300

    Creacion de Models

---
+++ backend/app/Models/Vendedor.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vendedor extends Model
+{
+    use HasFactory;
+
+    public function clientes()
+    {
+        return $this->hasMany(Cliente::class);
+    }
+}
